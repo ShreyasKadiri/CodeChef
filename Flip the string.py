625e77db68fcf621b49631f3906baa887329aa94
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-11-23 19:08:41 +0530

    Create Flip the string.py

---
+++ Flip the string.py
@@ -0,0 +1,22 @@
+# cook your dish here
+import sys
+if __name__=="__main__":
+ for i in range(int(input())):
+    x=input()
+    y=input()
+    a=[]
+    for i in x:
+        a.append(i)
+    b=[]
+    for i in y:
+        b.append(i)
+    c=0
+    for i in range(len(a)):
+        if a[i]==b[i]:
+            continue
+        p=i
+        while p<len(a) and (a[p]!=b[p]):
+            a[p]=b[p]
+            p=p+2
+        c=c+1
+    print(c)
