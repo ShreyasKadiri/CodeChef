fd5ad94ebea6dfad6b808d2f6fbd7d216ad0ae2e
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-31 17:58:34 +0530

    increment

---
+++ Chef and College Admisison.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int t;
+  cin>>t;
+  while(t--)
+  {
+      int r;
+      cin>>r;
+      int n;
+      cin>>n;
+      int a[n];
+      for(int i=0;i<n;i++)
+      cin>>a[i];
+      int f=n;
+      for(int i=0;i<n;i++)
+      {
+          if(a[i]>=r)
+          {
+              f=i+1;
+              break;
+          }
+      }
+      cout<<f<<endl;
+  }
+}
